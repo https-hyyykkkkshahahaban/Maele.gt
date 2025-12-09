65add7c560c1d034db1ecb5928af593b8dc13113
https-hyyykkkkshahahaban <stockxemzminecraft01@gmail.com>
2025-12-09 17:26:34 +0700

    Update inbox.js

--- api/inbox.js
+++ api/inbox.js
@@ -1,16 +1,16 @@
 export default async function handler(req, res) {
-    let token = global.token;
+    const token = global.token;
 
-    // kalau user ingin membuka satu email
+    // jika buka 1 email
     if (req.query.id) {
-        let response = await fetch(`https://api.mail.tm/messages/${req.query.id}`, {
+        let r = await fetch(`https://api.mail.tm/messages/${req.query.id}`, {
             headers: { Authorization: "Bearer " + token }
         });
-        let data = await response.json();
-        return res.status(200).json(data);
+        let d = await r.json();
+        return res.status(200).json(d);
     }
 
-    // ambil semua inbox
+    // ambil inbox
     let response = await fetch("https://api.mail.tm/messages", {
         headers: { Authorization: "Bearer " + token }
     });
