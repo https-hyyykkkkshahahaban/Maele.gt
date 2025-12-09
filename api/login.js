87515004d386783d5557318448f13d2445026847
https-hyyykkkkshahahaban <stockxemzminecraft01@gmail.com>
2025-12-09 17:30:27 +0700

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,22 +1,37 @@
 export default async function handler(req, res) {
+    // pilih akun berdasarkan query ?acc=1 atau ?acc=2
+    const acc = req.query.acc;
 
-    const realEmail = "aseplogimaja@comfythings.com";
-    const realPassword = "aseplogimaja";
+    const accounts = {
+        "1": {
+            email: "aseplogimaja@comfythings.com",
+            pass: "aseplogimaja"
+        },
+        "2": {
+            email: "EMAIL_ASLI_2",
+            pass: "PASSWORD_ASLI_2"
+        }
+    };
+
+    const selected = accounts[acc];
+
+    if (!selected) {
+        return res.status(400).json({ error: "Akun tidak ditemukan" });
+    }
 
     let response = await fetch("https://api.mail.tm/token", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
-            address: realEmail,
-            password: realPassword
+            address: selected.email,
+            password: selected.pass
         })
     });
 
     let data = await response.json();
 
-    // simpan token di server (tidak terlihat user)
+    // Simpan token di server (tidak terlihat user)
     global.token = data.token;
 
-    // kirim token palsu
-    res.status(200).json({ token: "SENSOR" });
+    return res.status(200).json({ status: "OK" });
 }
