506d2d887910105e67a45db2faaf2155ac74c402
https-hyyykkkkshahahaban <stockxemzminecraft01@gmail.com>
2025-12-09 17:30:47 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-function startLogin() {
-    // tampilkan viewer
-    document.getElementById("viewer").style.display = "block";
+function loginSelected() {
+    const selected = document.getElementById("emailSelect").value;
 
-    // panggil backend login (server yang simpan password asli)
-    fetch("/api/login")
-        .then(r => r.json())
-        .then(d => {
-            window.realToken = d.token; // token palsu "SENSOR"
-            console.log("Login backend OK");
+    // panggil login backend
+    fetch("/api/login?acc=" + selected)
+        .then(res => res.json())
+        .then(() => {
+            document.getElementById("viewer").style.display = "block";
+            loadInbox();
         });
 }
 
 async function loadInbox() {
-    let response = await fetch("/api/inbox");
-    let data = await response.json();
+    let res = await fetch("/api/inbox");
+    let data = await res.json();
 
     let box = document.getElementById("inbox");
     box.innerHTML = "";
@@ -26,18 +25,21 @@ async function loadInbox() {
     data["hydra:member"].forEach(msg => {
         let div = document.createElement("div");
         div.className = "email-item";
-        div.innerHTML = `<b>From:</b> ${msg.from.address}<br><b>Subject:</b> ${msg.subject}`;
+        div.innerHTML =
+            `<b>From:</b> ${msg.from.address}<br>
+             <b>Subject:</b> ${msg.subject}`;
         div.onclick = () => loadEmail(msg.id);
         box.appendChild(div);
     });
 }
 
 async function loadEmail(id) {
-    let response = await fetch("/api/inbox?id=" + id);
-    let data = await response.json();
+    let res = await fetch("/api/inbox?id=" + id);
+    let data = await res.json();
 
     let box = document.getElementById("emailContent");
     box.style.display = "block";
+
     box.innerHTML = `
         <h3>${data.subject}</h3>
         <p><b>From:</b> ${data.from.address}</p>
